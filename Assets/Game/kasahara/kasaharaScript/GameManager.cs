94dc1f821bd6a3e63758a1ad2f9e0d1c88b08434
KasaharaSota <souta.programer1201@gmail.com>
2024-05-24 14:14:13 +0900

    笠原のファイルの作成と、タイマーの処理

---
+++ Assets/Game/kasahara/kasaharaScript/GameManager.cs
@@ -0,0 +1,31 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private TimeManager timeManager;
+
+    private bool finished;
+
+    void Start()
+    {
+
+    }
+
+    public void Goal()
+    {
+        finished = true;
+    }
+
+    //Å@ÉQÅ[ÉÄÇèIóπÇµÇΩÇ©Ç«Ç§Ç©
+    public bool IsFinished()
+    {
+        return finished;
+    }
+
+}
