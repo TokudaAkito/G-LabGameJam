79f3f16d33a1ad8021eb0bd9ddb200fdabbc14a8
TokudaAkito <ukameiinfu2024128@gmail.com>
2024-05-24 15:12:37 +0900

    hpBar作成

---
+++ Assets/Menber/Tokuda/HealthSystem.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class HealthSystem : MonoBehaviour
+{
+    Animator anim;
+    public UnityEvent onDieCallback = new UnityEvent();
+
+    public int life = 100;
+
+    public Slider hpBar;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+        if (hpBar != null)
+        {
+            hpBar.value = life;
+        }
+    }
+
+    public void Damage(int damage)
+    {
+        if (life <= 0) return;
+
+        life -= damage;
+        if (hpBar != null)
+        {
+            hpBar.value = life;
+        }
+        if (life <= 0)
+        {
+
+        }
+    }
+}
