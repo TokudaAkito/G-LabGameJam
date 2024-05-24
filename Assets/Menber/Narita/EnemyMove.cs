d4fa80f02dd951aa04a64cc4a974226d3d0bb96d
NaritaShosei <onigirikun7171@gmail.com>
2024-05-24 16:21:58 +0900

    ItemGet

--- Assets/Menber/Narita/EnemyMove.cs
+++ Assets/Menber/Narita/EnemyMove.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class EnemyMove : MonoBehaviour
 {
-    public GameObject player; //①動かしたいオブジェクトをインスペクターから入れる
+    public GameObject enemy; //①動かしたいオブジェクトをインスペクターから入れる
     [SerializeField] float _enemySpeed = 1f;　//オブジェクトが自動で動くスピード調整
+    [SerializeField] float _moveRange = 7f;
     Vector3 movePosition;　//②オブジェクトの目的地を保存
     // Start is called before the first frame update
     void Start()
@@ -16,16 +17,16 @@ public class EnemyMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (movePosition == player.transform.position)  //②playerオブジェクトが目的地に到達すると、
+        if (movePosition == enemy.transform.position)  //②playerオブジェクトが目的地に到達すると、
         {
             movePosition = moveRandomPosition();  //②目的地を再設定
         }
-        this.player.transform.position = Vector3.MoveTowards(player.transform.position, movePosition, _enemySpeed * Time.deltaTime); 
+        this.enemy.transform.position = Vector3.MoveTowards(enemy.transform.position, movePosition, _enemySpeed * Time.deltaTime); 
         //①②playerオブジェクトが, 目的地に移動, 移動速度
     }
     private Vector3 moveRandomPosition()  // 目的地を生成、xとyのポジションをランダムに値を取得 
     {
-        Vector3 randomPosi = new Vector3(Random.Range(-7, 7), transform.position.y); //()を微調整
+        Vector3 randomPosi = new Vector3(Random.Range( -_moveRange, _moveRange), enemy.transform.position.y); //()を微調整
         return randomPosi;
     }
 }
