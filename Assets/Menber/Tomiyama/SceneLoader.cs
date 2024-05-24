a8dd32bf870dc8a336758d3890a494490a777295
AoiTomiyama <TadoO.o2209@gmail.com>
2024-05-24 16:40:55 +0900

    フェードイン、フェードアウトを行うPrefab[LoadSceneFadeOut]を追加

--- Assets/Menber/Tomiyama/SceneLoader.cs
+++ Assets/Menber/Tomiyama/SceneLoader.cs
@@ -1,18 +1,61 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneLoader : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Image _image = null;
+
+
+    private void Start()
     {
-        
+        StartCoroutine(WaitAndFadeOut(0, 2f));
+    }
+    private IEnumerator AlphaZeroToOne(float d, Action on_completed, bool isReversing = false)
+    {
+        if (!isReversing) _image.enabled = true;
+
+        var elapsedTime = 0.0f;
+        var color = _image.color;
+
+        while (elapsedTime < d) 
+        {
+            var elapsedRate = Mathf.Min(elapsedTime / d, 1f);
+            color.a = isReversing ? elapsedRate :1.0f - elapsedRate;
+            yield return null;
+            elapsedTime += Time.deltaTime;
+           _image.color = color;
+        }
+        if (isReversing ) _image.enabled = false;
+        if (on_completed != null) on_completed();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FadeIn(float d, Action on_complete = null)
     {
-        
+        StartCoroutine(AlphaZeroToOne(d, on_complete, true));
     }
+    private void FadeOut(float d, Action on_complete = null)
+    {
+        StartCoroutine(AlphaZeroToOne(d, on_complete));
+    }
+    public IEnumerator WaitAndFadeIn(float seconds, float fadingTime, string name)
+    {
+        yield return new WaitForSeconds(seconds);
+        FadeIn(fadingTime, () => SceneManager.LoadScene(name));
+    }
+
+    public IEnumerator WaitAndFadeOut(float seconds, float fadingTime)
+    {
+        yield return new WaitForSeconds(seconds);
+        FadeOut(fadingTime);
+    }
+
+    public void MoveToOtherScene(string sceneName)
+    {
+        StartCoroutine(WaitAndFadeIn(0.3f, 1.5f, sceneName));
+    }
+
 }
